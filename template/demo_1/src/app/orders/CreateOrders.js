a60e73ba9b737ecf8633dd1fe05a1ea4b044052d
Ankit Salian <ankitsalian9@gmail.com>
2022-04-03 17:17:13 +0530

    first commit

---
+++ template/demo_1/src/app/orders/CreateOrders.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CreateOrders = () => {
+    return (
+        <div className="row">
+            <div className="col-12 grid-margin">
+                <div className="card">
+                    <div className="card-body">
+                        <h4 className="card-title">Create Orders</h4>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CreateOrders;
